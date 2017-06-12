977e8b931586588078f47d4fd9740ebb57e29fca
jiangzhifei <jiangzhifei@eskygo.com>
2017-06-12 15:46:12 +0800

    add main class

---
+++ src/main/java/com/go/channel/web/QuickPayControllerFacade.java
@@ -0,0 +1,50 @@
+package com.go.channel.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jiang on 2017/6/12.
+ *<p>
+ *     快捷支付facade，屏蔽银行差异以及实现细节，提供快捷支付所需要的接口
+ *</p>
+ *
+ */
+@RestController
+public class QuickPayControllerFacade {
+
+
+    /**
+     * 签约
+     */
+    public void sign(){
+
+    }
+
+    /**
+     * 判断是否签约
+     */
+    public void isSigned(){
+
+    }
+    /**
+     * 支付
+     */
+    public void pay(){
+
+    }
+
+    /**
+     * 发送短信
+     */
+    public void sendSms(){
+
+    }
+
+    /**
+     * 解约
+     */
+    public void reAssign(){
+
+    }
+
+}
